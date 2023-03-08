c700b185e886b3478b3f694e75762ceccc45aa4a
SumithRaj <sumithraj05@SumithRaj.sr>
2023-03-08 09:40:36 +0530

    done

---
+++ ClientSide/src/components/Loading/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './Loading.css';
+
+function Loading(){
+    
+    return(
+        <div className="BgContainer">
+            <div className="Rotator"></div>
+        </div>
+    )
+}
+
+export default Loading;
