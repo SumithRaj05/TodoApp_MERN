1c4bd847aa091f42f7b322ac6de7d8a96ef8b3fe
SumithRaj <sumithraj05@SumithRaj.sr>
2023-03-08 11:47:45 +0530

    done

--- ClientSide/src/App.js
+++ ClientSide/src/App.js
@@ -8,14 +8,14 @@ import Login from './components/LoginForm/Login'
 
 function App() {
   // const auth = useContext(AuthContext);
-  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-  console.log(isLoggedIn)
+  const IsLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+  console.log(IsLoggedIn)
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Navigate to='/Login'/>}/>
         
-        <Route path='/:id'  element={ isLoggedIn? <HomePage/> : <Navigate to='/Login'/> }/>
+        <Route path='/:id'  element={ IsLoggedIn? <HomePage/> : <Navigate to='/Login'/> }/>
         
         <Route path='/Signup' element={ <Signup/> }/>
         <Route path='/Login' element={ <Login/> }/>
--- ClientSide/src/auth/Url.js
+++ ClientSide/src/auth/Url.js
@@ -1,4 +1,4 @@
-const URL = "https://weak-gray-boa-gear.cyclic.app";
-// const URL = "http://localhost:5000";
+// const URL = "https://weak-gray-boa-gear.cyclic.app";
+const URL = "http://localhost:5000";
 
 export default URL;
