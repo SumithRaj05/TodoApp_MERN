98a3fcc05981c05aa2f2beb5cb477a49ab44fe95
SumithRaj <sumithraj05@SumithRaj.sr>
2023-03-08 08:28:16 +0530

    done

--- ServerSide/Server.js
+++ ServerSide/Server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://srtaskmanager.netlify.app/Login');
+    res.setHeader('Access-Control-Allow-Origin', 'https://srtaskmanager.netlify.app/*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
