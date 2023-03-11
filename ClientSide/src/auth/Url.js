22635b3f45edb98eb2cca580283de812b6bcbb13
SumithRaj <sumithraj05@SumithRaj.sr>
2023-03-11 10:36:47 +0530

    done

--- ClientSide/src/auth/Url.js
+++ ClientSide/src/auth/Url.js
@@ -1,6 +1,6 @@
-// const URL = "https://weak-gray-boa-gear.cyclic.app";
+const URL = "https://weak-gray-boa-gear.cyclic.app";
 // const URL = "http://192.168.43.254:5000";
-const URL = "http://127.0.0.1:5000";
+// const URL = "http://127.0.0.1:5000";
 
 
 export default URL;
